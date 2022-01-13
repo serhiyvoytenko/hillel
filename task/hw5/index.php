5fbb28517467ab8f720a7c87ebb2b588980918db
Serhii <serhiy.voytenko@dn.pebs.biz>
2022-01-13 18:03:26 +0200

    start homework 5

---
+++ task/hw5/index.php
@@ -0,0 +1,6 @@
+<?php
+
+echo "<a href='createTable.php'>Create table</a><br>";
+echo "<a href='createUser.php'>Create user</a><br>";
+echo "<a href='delUser.php'>Delete user</a><br>";
+echo "<a href='listUser.php'>List user</a><br>";
