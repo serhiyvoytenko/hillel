c3aa1bee68b6cfd0e95fc7b96838af22e4c1d5fa
Serhii <serhiy.voytenko@dn.pebs.biz>
2022-01-17 20:02:09 +0200

    add config & pdo

---
+++ task/hw5/config.php
@@ -0,0 +1,6 @@
+<?php
+
+const DB_HOST = 'hillel_mysql_new';
+const DB_NAME = 'hillel_db';
+const DB_USER = 'hillel_user';
+const DB_PWD = 'hillel_pwd';
--- task/hw5/createTable.php
+++ task/hw5/createTable.php
@@ -1,1 +1,12 @@
 <?php
+
+include_once __DIR__."/config.php";
+
+$dsn = "mysql:dbname=".DB_NAME.";host=".DB_HOST;
+
+$dbh = new PDO($dsn, DB_USER, DB_PWD);
+var_dump($dbh);
+
+echo '<form action="" method="get">
+    <button type="submit" value="createTable">Create Table</button>
+    </form>';
