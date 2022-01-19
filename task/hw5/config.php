4af25e4aa5771cd9d7e4f849d1ac88ee03b5c35e
Serhii <serhiy.voytenko@dn.pebs.biz>
2022-01-19 19:23:51 +0200

    add pdo statement

--- task/hw5/config.php
+++ task/hw5/config.php
@@ -4,3 +4,4 @@ const DB_HOST = 'hillel_mysql_new';
 const DB_NAME = 'hillel_db';
 const DB_USER = 'hillel_user';
 const DB_PWD = 'hillel_pwd';
+
--- task/hw5/createTable.php
+++ task/hw5/createTable.php
@@ -4,8 +4,26 @@ include_once __DIR__."/config.php";
 
 $dsn = "mysql:dbname=".DB_NAME.";host=".DB_HOST;
 
-$dbh = new PDO($dsn, DB_USER, DB_PWD);
-var_dump($dbh);
+$opt = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+
+
+$dbh = new PDO($dsn, DB_USER, DB_PWD, $opt);
+
+$stmt = $dbh->query('SELECT * FROM cars');
+
+foreach ($stmt->fetch() as $row){
+//    var_dump($row);
+    echo $row.'<br>';
+}
+//$str = $dbh->query();
+
+var_dump($dbh, $stmt->fetchAll());
+
+
 
 echo '<form action="" method="get">
     <button type="submit" value="createTable">Create Table</button>
