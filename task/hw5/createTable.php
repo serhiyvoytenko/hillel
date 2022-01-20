1fd204edda6b185fc640fcee4c228e089a44a5c8
Serhii <serhiy.voytenko@dn.pebs.biz>
2022-01-20 12:05:04 +0200

    add style and create user

--- task/hw5/createTable.php
+++ task/hw5/createTable.php
@@ -5,12 +5,12 @@ include_once __DIR__ . "/config.php";
 $dsn = "mysql:dbname=" . DB_NAME . ";host=" . DB_HOST;
 
 $command = 'CREATE TABLE IF NOT EXISTS users (
-            id INTEGER PRIMARY KEY,
+            id INT AUTO_INCREMENT PRIMARY KEY,
             name VARCHAR (255) NOT NULL,
             surname VARCHAR (255) NOT NULL,
             age INT, 
             email VARCHAR (100) NOT NULL UNIQUE, 
-            phone VARCHAR (12) UNIQUE   
+            phone VARCHAR (12) UNIQUE  
             )';
 
 $dbh = new PDO($dsn, DB_USER, DB_PWD);
--- task/hw5/createUser.php
+++ task/hw5/createUser.php
@@ -1,1 +1,47 @@
 <?php
+
+include_once __DIR__ . "/config.php";
+
+$dsn = "mysql:dbname=" . DB_NAME . ";host=" . DB_HOST;
+
+$command = 'INSERT INTO
+    `users` (name, surname, age, email, phone)
+    VALUES
+        (:name, :surname, :age, :email, :phone)';
+
+$dbh = new PDO($dsn, DB_USER, DB_PWD);
+
+
+if (isset($_POST['name'])) {
+    $dbh->prepare($command);
+}
+
+echo <<<FORM
+    <form action='' method='post'>
+        <ul>
+            <li>
+                <label for="name">Name:</label>
+                <input type='text' id="name" name='name'>
+            </li>
+            <li>
+                <label for="surname">Surname:</label>
+                <input type='text' id="surname" name='surname'>
+            </li>
+            <li>
+                <label for="age">Age:</label>
+                <input type="number" id="age" name='age'>
+            </li>
+            <li>
+                <label for="email">Email:</label>
+                <input type='email' id="email" name='email'>
+            </li>
+            <li>
+                <label for="phone">Phone:</label>
+                <input type='text' id="phone" name='phone'>
+            </li>
+            <li>
+                <button type='submit'>Create user</button>
+            </li>
+        </ul>
+        </form>
+FORM;
