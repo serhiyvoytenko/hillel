c988d5ddf58d592e3f34664a380be7b85aa5522c
Serhii <serhiy.voytenko@dn.pebs.biz>
2022-01-25 09:20:16 +0200

    add filter post

--- task/hw5/createUser.php
+++ task/hw5/createUser.php
@@ -8,11 +8,16 @@ $command = 'INSERT INTO
     `users` (name, surname, age, email, phone)
     VALUES
         (:name, :surname, :age, :email, :phone)';
+$regexp = '/[a-zA-Z\s]/';
 
 $dbh = new PDO($dsn, DB_USER, DB_PWD);
 
-if (isset($_POST['name'])) {
-    $data['name'] = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+if (isset($_POST['name']) &&
+    filter_var($_POST['name'], FILTER_VALIDATE_REGEXP, $regexp) &&
+    filter_var($_POST['surname'], FILTER_VALIDATE_REGEXP, $regexp) &&
+    filter_var($_POST['age'], FILTER_VALIDATE_INT, array("options" => array("min_range" => 5, "max_range" => 100))) &&
+    filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) &&
+    filter_var($_POST['phone'], FILTER_VALIDATE_INT, array("options" => array("min_range" => 300000000000, "max_range" => 309999999999)))) {
 
     $stmt = $dbh->prepare($command);
     $stmt->execute($_POST);
