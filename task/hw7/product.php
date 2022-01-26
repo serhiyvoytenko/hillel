93536ee73f5473467f870d2c6bed7c9abb84ab4a
Serhii Voitenko <serhiy.voytenko@dn.pebs.biz>
2022-01-26 22:17:45 +0200

    hw7 end

--- task/hw7/product.php
+++ task/hw7/product.php
@@ -1,17 +1,36 @@
 <?php
 
-class Product {
-    public function get(name) {}
-    public function set(name, value) {}
+class Product
+{
+    public function get(name) {
+    }
+
+    public function set(name, value) {
+    }
 }
 
-class DbProduct {
-    public function save() {}
-    public function update() {}
-    public function delete() {}
-    public function show() {}
+class DbProduct
+{
+    public function save()
+    {
+    }
+
+    public function update()
+    {
+    }
+
+    public function delete()
+    {
+    }
+
+    public function show()
+    {
+    }
 }
 
-class PrintProduct{
-    public function print() {}
+class PrintProduct
+{
+    public function print()
+    {
+    }
 }
