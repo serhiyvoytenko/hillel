b2a5cd23c6cfce986d48015521a78ad64d567fca
Serhii <serhiy.voytenko@dn.pebs.biz>
2022-01-31 14:27:27 +0200

    hw10 finish

---
+++ task/hw10/taxi.php
@@ -0,0 +1,97 @@
+<?php
+
+use JetBrains\PhpStorm\Pure;
+
+interface GetTaxiData
+{
+    public function getTaxiModel(): string;
+    public function getTaxiPrice(): int;
+}
+
+class Mercedes implements GetTaxiData
+{
+
+    public function getTaxiModel(): string
+    {
+        return 'Mercedes';
+    }
+
+    public function getTaxiPrice(): int
+    {
+        return 100;
+    }
+}
+
+class Volkswagen implements GetTaxiData
+{
+
+    public function getTaxiModel(): string
+    {
+        return 'Volkswagen';
+    }
+
+    public function getTaxiPrice(): int
+    {
+        return 50;
+    }
+}
+
+class Lada implements GetTaxiData
+{
+
+    public function getTaxiModel(): string
+    {
+        return 'Lada';
+    }
+
+    public function getTaxiPrice(): int
+    {
+        return 20;
+    }
+}
+
+abstract class Delivery
+{
+    abstract public function createDelivery(): GetTaxiData;
+}
+
+class EconomDelivery extends Delivery
+{
+    #[Pure] public function createDelivery(): GetTaxiData
+    {
+        return new Lada();
+    }
+}
+
+class StandartDelivery extends Delivery
+{
+    #[Pure] public function createDelivery(): GetTaxiData
+    {
+        return new Volkswagen();
+    }
+}
+
+class LuxDelivery extends Delivery
+{
+    #[Pure] public function createDelivery(): GetTaxiData
+    {
+        return new Mercedes();
+    }
+}
+
+function getTaxi(Delivery $deliver): GetTaxiData
+{
+    return $deliver->createDelivery();
+}
+
+$car = getTaxi(new LuxDelivery());
+
+var_dump($car->getTaxiModel(), $car->getTaxiPrice());
+
+$car1 = getTaxi(new EconomDelivery());
+
+var_dump($car1->getTaxiModel(), $car1->getTaxiPrice());
+
+$car2 = getTaxi(new StandartDelivery());
+
+var_dump($car2->getTaxiModel(), $car2->getTaxiPrice());
