faccc91d600d2afb527be07d46fcefd51096a06d
Serhii Voitenko <serhiy.voytenko@dn.pebs.biz>
2022-02-08 19:23:46 +0200

    hw13 sql DATE fix

--- task/hw13/sql/create blog_users.sql
+++ task/hw13/sql/create blog_users.sql
@@ -4,7 +4,7 @@ CREATE TABLE blog_users
     username   VARCHAR (50)  NOT NULL ,
     last_name  VARCHAR (200) NULL ,
     first_name VARCHAR (200) NULL ,
-    birthday   DATA         NULL ,
+    birthday   DATE         NULL ,
     email      VARCHAR (100) NOT NULL UNIQUE ,
     password   VARCHAR (100) NOT NULL UNIQUE
 );
