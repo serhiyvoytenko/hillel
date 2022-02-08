853bd12d65c6e8f3a1b27849a9f72cc8c9e48b71
Serhii <serhiy.voytenko@dn.pebs.biz>
2022-02-08 18:39:24 +0200

    hw 13 correct DB config

--- task/hw13/Config/constants.php
+++ task/hw13/Config/constants.php
@@ -1,6 +1,6 @@
 <?php
 
-const DB_HOST = '127.0.0.1';
+const DB_HOST = 'db';
 const DB_NAME = 'hillel_db';
 const DB_USER = 'hillel_user';
 const DB_PASSWORD = 'hillel_pwd';
--- task/hw13/Core/DB.php
+++ task/hw13/Core/DB.php
@@ -14,7 +14,7 @@ class DB
     {
         if (static::$connection === null) {
             static::$connection = new PDO(
-                "mysql:host=".DB_HOST.";dbname=".DB_NAME.";".DB_USER.",".DB_PASSWORD
+                "mysql:host=".DB_HOST.";dbname=".DB_NAME.";",DB_USER,DB_PASSWORD
             );
         }
         return static::$connection;
